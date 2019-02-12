f7cde70116b7b724979a88046097768014024c97
Luke Logan <llogan382@gmail.com>
2019-02-12 13:24:41 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path'),
+  MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+  UglifyJSPlugin = require('uglifyjs-webpack-plugin'),
+  OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin'),
+  BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    frontend: ['babel-polyfill', './src/index.js'],
+    customizer: './src/customizer.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name]-bundle.js'
+  },
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        exclude: /node_modules/,
+        test: /\.jsx$/,
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: '../style.css' }),
+    new BrowserSyncPlugin({
+      files: '**/*.php',
+      injectChanges: true,
+      proxy: 'http://loganwebdevwebpack.local'
+    })
+  ],
+  optimization: {
+    minimizer: [new UglifyJSPlugin(), new OptimizeCssAssetsPlugin()]
+  }
+};
